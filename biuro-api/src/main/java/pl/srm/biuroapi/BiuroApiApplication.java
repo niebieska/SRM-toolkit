a3a34fdf334c8413eeea2510b938e6cf02f2dfb9
Paulina Wątroba <paulina@watroba.com.pl>
2026-05-10 18:53:38 +0200

    Initialize project structure

---
+++ biuro-api/src/main/java/pl/srm/biuroapi/BiuroApiApplication.java
@@ -0,0 +1,13 @@
+package pl.srm.biuroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiuroApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiuroApiApplication.class, args);
+    }
+
+}
